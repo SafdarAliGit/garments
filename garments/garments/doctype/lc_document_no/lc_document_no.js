38f03c40d3e48b80905dd83073f5c9eee417ff62
safdar211ali <safdar211@hotmail.com>
2024-10-05 19:44:47 +0500

    updated

---
+++ garments/garments/doctype/lc_document_no/lc_document_no.js
@@ -0,0 +1,8 @@
+// Copyright (c) 2024, Unilink Enterprise and contributors
+// For license information, please see license.txt
+
+frappe.ui.form.on('LC Document No', {
+	// refresh: function(frm) {
+
+	// }
+});
